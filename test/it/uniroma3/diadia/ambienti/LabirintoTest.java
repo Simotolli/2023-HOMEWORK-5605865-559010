0212399fb5d6db568f55f0236bd3e37055b721eb
Simotolli <129169691+Simotolli@users.noreply.github.com>
2023-03-28 12:42:52 +0200

    Add files via upload

---
+++ test/it/uniroma3/diadia/ambienti/LabirintoTest.java
@@ -0,0 +1,27 @@
+package it.uniroma3.diadia.ambienti;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LabirintoTest {
+	private Labirinto labirinto;
+
+    @Before
+    public void setUp() {
+    	this.labirinto = new Labirinto();
+    }
+	
+	@Test
+	public void testVerificaCorrettaStanzaVincente() {
+		assertEquals("Biblioteca",this.labirinto.getStanzaVincente().getNome());
+	}
+	@Test
+	public void testVerificaCorrettaStanzaCorrente() {
+		assertEquals("Atrio",this.labirinto.getStanzaCorrente().getNome());
+	}
+	
+	
+
+}
